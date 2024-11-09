5d4b2c8f9dcde7199f083f42d04bcebd4a4eeaf7
CarlesRojas <carlesrojas@outlook.com>
2024-11-09 10:20:42 +0100

    cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,14 @@
 import type { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        extend: {
+            screens: {
+                mouse: { raw: "(hover: hover)" },
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [animate],
 } satisfies Config;
